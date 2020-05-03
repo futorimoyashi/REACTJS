d4d6c1a2cc5dfa97d78499b2fd1958a1e8de96b4
futorimoyashi <jamekin196@gmail.com>
2020-05-03 23:32:39 +0900

    loop to render

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,30 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TodoItem title="eating"/>
-        <TodoItem title="sleeping"/>
-        <TodoItem title="eating and sleeping"/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.todoItems = ['eating', 'sleeping', 'eating and sleeping'];
+    this.contents = [
+      {
+        company: 'Alfreds Futterkiste',
+        contact: 'Maria Anders',
+        country: 'Germany'
+      },
+      {
+        company: 'Centro comercial Moctezuma',
+        contact: 'Francisco Chang',
+        country: 'Mexico'
+      },
+      {
+        company: 'Ernst Handel',
+        contact: 'Roland Mendel',
+        country: 'Australia'
+      },
+      {
+        company: 'Island Trading',
+        contact: 'Helen Bennett',
+        country: 'UK'
+      }, 
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+          {this.todoItems.map((item, index) => <TodoItem key={index}  title={item} />)}   
+          <table>
+            <tr>
+              <th>Company</th>
+              <th>Contact</th>
+              <th>Country</th>
+            </tr>
+            {this.contents.map((item, index) => <tr><td>{item.company}</td><td>{item.contact}</td><td>{item.country}</td></tr>)}
+          </table>
+      </div>
+    );
+  }
 }
 
 export default App;
