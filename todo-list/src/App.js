537516a1e00ab1329c5562ecd47b4e661f3caaf6
futorimoyashi <jamekin196@gmail.com>
2020-05-11 23:34:15 +0900

    todo app

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,32 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
-// import TodoItem from './components/TodoItem'
+import TodoItem from './components/TodoItem'
+import tick from './img/tick.svg'
 // import CpnImage from './components/CpnImage'
 // import TrafficLight from './components/TrafficLight'
 // import BindingForm from './components/BindingForm'
 // import ClikMe from './components/ClickMe'
-import UpdateNum from './components/UpdateNum'
+// import UpdateNum from './components/UpdateNum'
 
 // const RED = 0;
 // const YELLOW = 1;
 // const GREEN = 2;
 class App extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     todoItems: [
-  //       {title: 'eating', isComplete: false}, 
-  //       {title: 'sleeping', isComplete: false}, 
-  //       {title: 'eating and sleeping', isComplete: true}
-  //     ]
-  //   };
+  constructor() {
+    super();
+    this.state = {
+      newItem: '',
+      todoItems: [
+        {title: 'eating', isComplete: false}, 
+        {title: 'sleeping', isComplete: false}, 
+        {title: 'eating and sleeping', isComplete: true}
+      ]
+    }
+    this.onKeyUp = this.onKeyUp.bind(this);
+    this.onChange = this.onChange.bind(this);
     // this.state = {currentColor: RED};
     // setInterval(() => {
     //     this.setState({
     //         currentColor: this.getNextColor(this.state.currentColor)
     //     })
     // }, 1000)
-  // }
+  }
 
   // getNextColor(color) {
   //   switch(color) {
@@ -41,18 +45,69 @@ class App extends Component {
   //   prevTodoItems[index].isComplete = !prevTodoItems[index].isComplete
   //   this.setState({prevTodoItems})
   // }
+  onItemClick(item) {
+    return(event) => {
+      const isComplete = item.isComplete
+      const { todoItems } = this.state
+      const index = todoItems.indexOf(item)
+      this.setState({
+        todoItems: [
+          ...todoItems.slice(0, index),
+          { ...item, isComplete: !isComplete},
+          ...todoItems.slice(index + 1)
+        ]
+      })
+    }
+  }
+
+  onKeyUp(event) {
+    if(event.keyCode === 13) {
+      let text = event.target.value;
+      if(!text) {return;}
+      text = text.trim();
+      if(!text) {return;}
+      this.setState({
+        newItem: '',
+        todoItems: [
+          {title: text, isComplete: false},
+          ...this.state.todoItems
+        ]
+      });
+    }
+  }
+
+  onChange(event) {
+    this.setState({
+      newItem: event.target.value
+    })
+  }
 
   render() {
     // const { currentColor } = this.state
+    const { todoItems, newItem } = this.state
     return (
       <div className="App">
-          {/* {this.state.todoItems.map((item, index) => 
-            <TodoItem key={index}  item={item} onClick={() => this.onItemClick(index)} /> )} */}
+        <div className="Header">
+          <img src={tick} width={32} height={32}/>
+          <input 
+            type="text" 
+            placeholder="Add a new item"
+            value={newItem}
+            onChange={this.onChange} 
+            onKeyUp={this.onKeyUp}/>
+        </div>
+        {todoItems.map((item, index) => 
+          <TodoItem 
+            key={index}  
+            item={item} 
+            // onClick={() => this.onItemClick(index)} 
+            onClick={this.onItemClick(item)}
+        /> )}
           {/* {<CpnImage />} */}
           {/* <TrafficLight currentColor={currentColor}/> */}
           {/* {<BindingForm />} */}
           {/* {<ClikMe />} */}
-          {<UpdateNum />}
+          {/* {<UpdateNum />} */}
       </div>
     );
   }
--- todo-list/src/components/TodoItem.js
+++ todo-list/src/components/TodoItem.js
@@ -1,15 +1,19 @@
 import React, { Component} from 'react'
+import classNames from 'classnames'
 import './TodoItem.css'
+import checkImg from '../img/check.svg'
+import checkCompleteImg from '../img/check-complete.svg'
 
 class TodoItem extends Component {
     render() {
         const { item, onClick } = this.props;
-        let className = 'TodoItem';
+        let url = checkImg
         if(item.isComplete) {
-            className += ' TodoItem-complete'
+            url = checkCompleteImg
         }
         return (
-            <div className={className} onClick={onClick}>
+            <div className={classNames('TodoItem', {'TodoItem-complete': item.isComplete})}>
+                <img onClick={onClick} src={url} width="32" height="32"/>
                 <p>{this.props.item.title}</p>
             </div>
         );
