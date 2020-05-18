b99d644cc7413880dea9f2db15ca762ace02de1b
futorimoyashi <jamekin196@gmail.com>
2020-05-18 17:28:54 +0900

    router feature

--- demo-children/src/App.js
+++ demo-children/src/App.js
@@ -1,15 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Accordion from './components/Accordion'
+// import Accordion from './components/Accordion'
+// import { Button } from 'reactstrap'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <Accordion heading="heading">
-        Children
-      </Accordion>
-    </div>
-  );
+const index = () => <h2>Index</h2>
+const about = () => <h2>About</h2>
+
+class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.inputElement = React.createRef();
+  // }
+
+  // componentDidMount() {
+  //   this.inputElement.current.focus();
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        {/* <Accordion heading="heading">
+          Children
+        </Accordion> */}
+        {/* <input type="text" ref={this.inputElement}/> */}
+        {/* <Button color="primary">primary</Button>{' '}
+        <Button color="secondary">secondary</Button>{' '}
+        <Button color="success">success</Button>{' '}
+        <Button color="info">info</Button>{' '}
+        <Button color="warning">warning</Button>{' '}
+        <Button color="danger">danger</Button>{' '}
+        <Button color="link">link</Button> */}
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+          <Route path="/" exact component={index} />
+          <Route path="/about" exact component={about} />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
