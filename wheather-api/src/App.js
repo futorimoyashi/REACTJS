185b6fa71904a92bc651f15326c8127c28db9b7e
futorimoyashi <jamekin196@gmail.com>
2020-05-30 01:02:35 +0900

    create wheather api app

---
+++ wheather-api/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ShowOff from './Component/ShowOff'
+
+function App() {
+  return (
+    <div className="App">
+      <ShowOff />
+    </div>
+  );
+}
+
+export default App;
