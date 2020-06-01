5b4d5f8b7f50fd8f1cb83074d3150b9bf340d719
futorimoyashi <jamekin196@gmail.com>
2020-06-01 18:03:59 +0900

    create pagination feature

--- wheather-api/src/App.js
+++ wheather-api/src/App.js
@@ -1,10 +1,53 @@
-import React from 'react';
-import ShowOff from './Component/ShowOff'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+import queryString from 'query-string'
+
+import PostList from './Component/PostList'
+import Pagination from './Component/Pagination'
 
 function App() {
+  const [postList, setPostList] = useState([]);
+  const [pagination, setPagination] = useState({
+    _page: 1,
+    _limit: 10,
+    _totalRows: 11,
+  })
+  const [filter, setFilter] = useState({
+    _limit: 10,
+    _page: 1,
+  })
+
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        const paramString = queryString.stringify(filter)
+        axios.get(`http://js-post-api.herokuapp.com/api/posts?${paramString}`)
+          .then(function(res) {
+            setPostList(res.data.data)
+            setPagination(res.data.pagination)
+          })
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+    fetchPostList()
+  }, [filter])
+
+  function handlePageChange(newPage) {
+    console.log(newPage)
+    setFilter({
+      ...filter,
+      _page: newPage
+    })
+  }
+
   return (
     <div className="App">
-      <ShowOff />
+      <PostList posts={postList} />
+      <Pagination 
+        pagination={pagination}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 }
