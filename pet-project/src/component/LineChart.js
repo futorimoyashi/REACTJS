247727f006e29c534ea2e48ab0fd99b9e8c2bbf1
futorimoyashi <jamekin196@gmail.com>
2020-07-03 13:03:55 +0900

    precipitation chart

--- pet-project/src/component/LineChart.js
+++ pet-project/src/component/LineChart.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import { Line } from "react-chartjs-2";
+import { Row, Col } from 'reactstrap';
 
 export default function(props) {
     const [data, setData] = useState([])
+    const [precipitation, setPrecipitation] = useState([])
 
     // function GetImg(id) {
     //     if(id > 38)  var img = require(`../icon/38.png`)
@@ -10,28 +13,62 @@ export default function(props) {
     //     return img
     // }
 
+    function getHour(date) {
+        var d = new Date(date)
+    
+        return `${d.getHours()}`
+    }
+
     useEffect(() => {
         async function fetchData() {
+
             const url = 'https://dataservice.accuweather.com/forecasts/v1/hourly/12hour/' + 
                         props.id + 
                         '?language=vi&apikey=' + 
                         'gHuEn9ghiy20CHSHAJ4ccgWcdU0XWkGS'
-            try {
-                axios.get(url)
-                    .then(res => setData(res.data))
-            } catch (error) {
-                console.log(error.message)
-            }
+            const res = await axios.get(url)
+
+            const listHours = []
+            const listPrecipitation = []
+
+            res.data.map(x => {
+                listHours.push(getHour(x.DateTime))
+                listPrecipitation.push(x.PrecipitationProbability)
+            })
+
+            setData(listHours)
+            setPrecipitation(listPrecipitation)
         }
         fetchData()
-    }, [])
+    }, [props.id])
 
     return (
-        <div>
-            <ul>
-                {data.map(x => <li>{x.IconPhrase}</li>)}
-            </ul>
-        </div>
-        // <p>Nothing</p>
+        <Row>
+            <Col sm={{size: 6, offset: 0}}>
+                <Line
+                    data={{
+                    labels: data,
+                    datasets: [
+                        {
+                        data: precipitation,
+                        label: "Precipitation Probability",
+                        borderColor: "#3e95cd",
+                        fill: false
+                        }
+                    ]
+                    }}
+                    options={{
+                    title: {
+                        display: true,
+                        text: "Precipitation Probability in next 12 hours"
+                    },
+                    legend: {
+                        display: true,
+                        position: "bottom"
+                    }
+                    }}
+                />
+            </Col>
+        </Row>
     )
 }
